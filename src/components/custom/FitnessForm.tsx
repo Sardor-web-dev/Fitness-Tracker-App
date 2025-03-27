5568c85cc514e69a4992210265c1bdc8686bd3c5
SarikViper <dzamolovsardor5@gmail.com>
2025-03-28 02:08:28 +0500

    not done adding logic

--- src/components/custom/FitnessForm.tsx
+++ src/components/custom/FitnessForm.tsx
@@ -1,9 +1,67 @@
-const FitnessForm = () => {
-    return (
-        <>
-        
-        </>
-    );
+import { useApi } from "@/hooks/UseApi";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { method } from "@/types/methodsApi";
+
+interface FitnessFormProps {
+  setModal: (value: boolean) => void;
 }
- 
+const FitnessForm = ({ setModal }: FitnessFormProps) => {
+  const { register, handleSubmit } = useForm();
+  const { fetchData } = useApi(import.meta.env.VITE_PUBLIC_API);
+
+  const onSubmit: SubmitHandler<Record<string, any>> = (name) => {
+
+    fetchData("/fitness", method.post, name).then((res) =>
+      console.log(res?.data)
+    );
+    setModal(false);
+  };
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black/50">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-red-500 w-[400px] h-[400px] p-6 rounded-lg shadow-lg flex flex-col gap-4"
+      >
+        <Label className="text-white text-lg">Enter Exercise</Label>
+        <Input
+          {...register("name", { required: true })}
+          type="text"
+          placeholder="Enter exercise"
+          className="p-3 rounded-md text-black border border-gray-300 w-full"
+        />
+        <Input
+          {...register("description", { required: true })}
+          type="text"
+          placeholder="Enter exercise"
+          className="p-3 rounded-md text-black border border-gray-300 w-full"
+        />
+        <Input
+          {...register("duration", { required: true })}
+          type="text"
+          placeholder="Enter exercise"
+          className="p-3 rounded-md text-black border border-gray-300 w-full"
+        />
+        <div className="flex justify-between">
+          <Button type="submit">Add Exercise</Button>
+          <Button onClick={() => setModal(false)} type="button">
+            Cancel
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
 export default FitnessForm;
+
+// {
+//     "id": "11",
+//     "name": "Zumba",
+//     "description": "Aerobic exercise that uses dance moves to burn calories and build strength.",
+//     "duration": 60,
+//     "caloriesBurned": 150
+//   }
--- src/components/custom/FitnessList.tsx
+++ src/components/custom/FitnessList.tsx
@@ -2,6 +2,8 @@ import { useApi } from "@/hooks/UseApi";
 import { method } from "@/types/methodsApi";
 import { useEffect, useState } from "react";
 import FitnessItem from "./FitnessItem";
+import { Button } from "../ui/button";
+import FitnessForm from "./FitnessForm";
 
 export interface item{
   id: number;
@@ -14,6 +16,7 @@ export interface item{
 const FitnessList = () => {
   const { fetchData } = useApi(import.meta.env.VITE_PUBLIC_API);
   const [fitness, setFitness] = useState<item[]>([]);
+  const [modal, setModal] = useState<boolean>(false);
   
   useEffect(() => {
     fetchData("/fitness", method.get)
@@ -25,11 +28,15 @@ const FitnessList = () => {
   };
   return (
     <>
+    <center>
     <h1 className="text-center text-4xl font-bold">Fitness List</h1>
+    <Button onClick={() => setModal(true)} className="cursor-pointer">Add Fitness Exsercise</Button>
+    </center>
     <center className="grid grid-cols-3 gap-4 p-20">
         {fitness.map((item: item) => (
           <FitnessItem key={item.id} {...item} onDelete={handleDeleteItem}/>
         ))}
+        {modal && <FitnessForm setModal={setModal}/>  }       
     </center>
     </>
   );
