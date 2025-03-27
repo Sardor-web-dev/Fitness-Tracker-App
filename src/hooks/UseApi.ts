66286a9c25a2ffd55eff49f68cd97eb66e2cf8cf
SarikViper <dzamolovsardor5@gmail.com>
2025-03-27 21:55:13 +0500

    added db.json and custom hook

---
+++ src/hooks/UseApi.ts
@@ -0,0 +1,34 @@
+import { method } from "@/types/methodsApi";
+import axios, { AxiosRequestHeaders } from "axios";
+import { useCallback, useState } from "react";
+
+export const useApi = (baseUrl: string) => {
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const fetchData = useCallback(
+    async (
+      path: string,
+      method: method,
+      body?: unknown,
+      headers?: AxiosRequestHeaders,
+    ) => {
+      setLoading(true);
+      try {
+        const res = await axios[method](baseUrl + path, {
+          data: body,
+          headers: headers,
+        });
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Something went wrong");
+        }
+        setLoading(false);
+        return res;
+      } catch (e) {
+        setError(true);
+        setLoading(false);
+      }
+    },
+    [baseUrl]
+  );
+  return { fetchData, error, loading };
+};
